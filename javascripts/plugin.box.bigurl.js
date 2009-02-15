f8dcdf35e57144ec311774594769e8ef74821e37
choonkeat <choonkeat@gmail.com>
2009-02-14 17:29:39 -0800

    introducing plugin to expand urls (on mouseover) (requires serverside support)

---
+++ javascripts/plugin.box.bigurl.js
@@ -0,0 +1,24 @@
+/*
+ * You'd need to implement your own server-side component, and configure it
+ * like settings.urls.big_url = "http://myserver.com/bigurls?url={query}"
+ *
+ * For example, in Rails:
+ *  url = URI.parse(params[:url])
+ *  res = Net::HTTP.start(url.host, url.port) {|http|
+ *    http.get([url.path, url.query].compact.join('?'), "User-Agent" => request.user_agent)
+ *  }
+ *  render :text => res['location']
+ */
+(function() {
+  function bigurl_onhover(event) {
+    var link = event.target;
+    if (!link.title && link.href.match(/(is\.gd|tinyurl\.com|bit\.ly|snipurl\.com)\//)) {
+      $.get(settings.urls.big_url.supplant({query: encodeURIComponent(link.href)}), function(expanded_url) {
+        link.href = link.title = expanded_url;
+        var match = expanded_url.match(/^\w+\:\/\/(.+)$/);
+        if (match[1]) link.innerHTML = '&raquo;' + match[1].substring(0, link.innerHTML.length-4) + '&laquo;';
+      });
+    }
+  }
+  $(document.body).mouseover(bigurl_onhover);
+})();
