3e005d6490bb86796d37f91742886d2f7acb220c
choonkeat <choonkeat@gmail.com>
2009-03-16 00:44:47 -0700

    Caret position fix

--- javascripts/plugin.box.reply.js
+++ javascripts/plugin.box.reply.js
@@ -1,4 +1,20 @@
 (function() {
+  // thanks http://blog.josh420.com/archives/2007/10/setting-cursor-position-in-a-textbox-or-textarea-with-javascript.aspx
+  function setCaretPosition(elem, caretPos) {
+    if (elem != null) {
+      if (elem.createTextRange) {
+        var range = elem.createTextRange();
+        range.move('character', caretPos);
+        range.select();
+      } else {
+        if (elem.selectionStart != null) {
+          elem.focus();
+          elem.setSelectionRange(caretPos, caretPos);
+        } else
+          elem.focus();
+      }
+    }
+  }
   var status_form_html =
   "<div class=\"okay\" style=\"display: block; float: " +
   "left;\">\n  <form action=\"{update_status_url}\" me" +
@@ -26,12 +42,16 @@
     this.add_menu_action("Reply", tweet_json.html_element, function(index, tweet_json) {
       update_status_form.insertAfter($('.menu:first', tweet_json.html_element)).show();
       $("input[name='in_reply_to_status_id']").val(tweet_json.id);
-      $("textarea[name='status']").val("@" + tweet_json.user.screen_name + " ").focus();
+      var textarea = $("textarea[name='status']");
+      textarea.val("@" + tweet_json.user.screen_name + " ");
+      setCaretPosition(textarea[0], textarea.val().length);
     });
     this.add_menu_action("Direct message", tweet_json.html_element, function(index, tweet_json) {
       update_status_form.insertAfter($('.menu:first', tweet_json.html_element)).show();
       $("input[name='in_reply_to_status_id']").val("");
-      $("textarea[name='status']").val("d " + tweet_json.user.screen_name + " ").focus();
+      var textarea = $("textarea[name='status']");
+      textarea.val("d " + tweet_json.user.screen_name + " ");
+      setCaretPosition(textarea[0], textarea.val().length);
     });
   });
 })();
